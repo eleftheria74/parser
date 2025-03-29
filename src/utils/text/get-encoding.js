5ec4a49252b96740759b22b8a448b3d12248977b
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-30 00:55:03 +0200

    Update get-encoding.js

--- src/utils/text/get-encoding.js
+++ src/utils/text/get-encoding.js
@@ -1,10 +1,9 @@
-import iconv from 'iconv-lite';
-import { DEFAULT_ENCODING, ENCODING_RE } from './constants';
+const iconv = require('iconv-lite');
+const { DEFAULT_ENCODING, ENCODING_RE } = require('./constants');
 
-// check a string for encoding; this is
-// used in our fetchResource function to
-// ensure correctly encoded responses
-export default function getEncoding(str) {
+// Check a string for encoding; this is used in our fetchResource function
+// to ensure correctly encoded responses
+function getEncoding(str) {
   let encoding = DEFAULT_ENCODING;
   const matches = ENCODING_RE.exec(str);
   if (matches !== null) {
@@ -15,3 +14,5 @@ export default function getEncoding(str) {
   }
   return encoding;
 }
+
+module.exports = getEncoding;
