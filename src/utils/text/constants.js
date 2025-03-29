7028e69c68e79d75692c89597eb8b7778c3e5425
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-30 00:39:27 +0200

    Update constants.js

--- src/utils/text/constants.js
+++ src/utils/text/constants.js
@@ -1,28 +1,23 @@
-// An expression that looks to try to find the page digit within a URL, if
-// it exists.
-// Matches:
-//  page=1
-//  pg=1
-//  p=1
-//  paging=12
-//  pag=7
-//  pagination/1
-//  paging/88
-//  pa/83
-//  p/11
-//
-// Does not match:
-//  pg=102
-//  page:2
-export const PAGE_IN_HREF_RE = new RegExp(
+// An expression that looks to try to find the page digit within a URL, if it exists.
+const PAGE_IN_HREF_RE = new RegExp(
   '(page|paging|(p(a|g|ag)?(e|enum|ewanted|ing|ination)))?(=|/)([0-9]{1,3})',
   'i'
 );
 
-export const HAS_ALPHA_RE = /[a-z]/i;
+const HAS_ALPHA_RE = /[a-z]/i;
 
-export const IS_ALPHA_RE = /^[a-z]+$/i;
-export const IS_DIGIT_RE = /^[0-9]+$/i;
+const IS_ALPHA_RE = /^[a-z]+$/i;
+const IS_DIGIT_RE = /^[0-9]+$/i;
 
-export const ENCODING_RE = /charset=([\w-]+)\b/;
-export const DEFAULT_ENCODING = 'utf-8';
+const ENCODING_RE = /charset=([\w-]+)\b/;
+const DEFAULT_ENCODING = 'utf-8';
+
+module.exports = {
+  PAGE_IN_HREF_RE,
+  HAS_ALPHA_RE,
+  IS_ALPHA_RE,
+  IS_DIGIT_RE,
+  ENCODING_RE,
+  DEFAULT_ENCODING
+};
+
