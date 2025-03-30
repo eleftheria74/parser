40d36c33b763b5857cc3f616b7694d075cf39b45
actions-user <actions@github.com>
2025-03-30 17:14:40 +0000

    fix: convert 'export * from' to CommonJS

--- src/extractors/custom/index.js
+++ src/extractors/custom/index.js
@@ -1,143 +1,289 @@
-export * from './blogspot.com';
-export * from './nymag.com';
-export * from './wikipedia.org';
-export * from './twitter.com';
-export * from './www.nytimes.com';
-export * from './www.theatlantic.com';
-export * from './www.newyorker.com';
-export * from './www.wired.com';
-export * from './www.msn.com';
-export * from './www.yahoo.com';
-export * from './www.buzzfeed.com';
-export * from './fandom.wikia.com';
-export * from './www.littlethings.com';
-export * from './www.politico.com';
-export * from './deadspin.com';
-export * from './www.broadwayworld.com';
-export * from './www.apartmenttherapy.com';
-export * from './medium.com';
-export * from './www.tmz.com';
-export * from './www.washingtonpost.com';
-export * from './www.huffingtonpost.com';
-export * from './newrepublic.com';
-export * from './money.cnn.com';
-export * from './www.theverge.com';
-export * from './www.cnn.com';
-export * from './www.aol.com';
-export * from './www.youtube.com';
-export * from './www.theguardian.com';
-export * from './www.sbnation.com';
-export * from './www.bloomberg.com';
-export * from './www.bustle.com';
-export * from './www.npr.org';
-export * from './www.recode.net';
-export * from './qz.com';
-export * from './www.dmagazine.com';
-export * from './www.reuters.com';
-export * from './mashable.com';
-export * from './www.chicagotribune.com';
-export * from './www.vox.com';
-export * from './news.nationalgeographic.com';
-export * from './www.nationalgeographic.com';
-export * from './www.latimes.com';
-export * from './pagesix.com';
-export * from './thefederalistpapers.org';
-export * from './www.cbssports.com';
-export * from './www.msnbc.com';
-export * from './www.thepoliticalinsider.com';
-export * from './www.mentalfloss.com';
-export * from './abcnews.go.com';
-export * from './www.nydailynews.com';
-export * from './www.cnbc.com';
-export * from './www.popsugar.com';
-export * from './observer.com';
-export * from './people.com';
-export * from './www.usmagazine.com';
-export * from './www.rollingstone.com';
-export * from './247sports.com';
-export * from './uproxx.com';
-export * from './www.eonline.com';
-export * from './www.miamiherald.com';
-export * from './www.refinery29.com';
-export * from './www.macrumors.com';
-export * from './www.androidcentral.com';
-export * from './www.si.com';
-export * from './www.rawstory.com';
-export * from './www.cnet.com';
-export * from './www.today.com';
-export * from './www.al.com';
-export * from './www.thepennyhoarder.com';
-export * from './www.westernjournalism.com';
-export * from './www.americanow.com';
-export * from './sciencefly.com';
-export * from './hellogiggles.com';
-export * from './thoughtcatalog.com';
-export * from './www.inquisitr.com';
-export * from './www.nbcnews.com';
-export * from './fortune.com';
-export * from './www.linkedin.com';
-export * from './obamawhitehouse.archives.gov';
-export * from './www.opposingviews.com';
-export * from './www.prospectmagazine.co.uk';
-export * from './forward.com';
-export * from './www.qdaily.com';
-export * from './gothamist.com';
-export * from './www.fool.com';
-export * from './www.slate.com';
-export * from './ici.radio-canada.ca';
-export * from './www.fortinet.com';
-export * from './www.fastcompany.com';
-export * from './blisterreview.com';
-export * from './news.mynavi.jp';
-export * from './clinicaltrials.gov';
-export * from './github.com';
-export * from './www.reddit.com';
-export * from './otrs.com';
-export * from './www.ossnews.jp';
-export * from './buzzap.jp';
-export * from './www.asahi.com';
-export * from './www.sanwa.co.jp';
-export * from './www.elecom.co.jp';
-export * from './scan.netsecurity.ne.jp';
-export * from './jvndb.jvn.jp';
-export * from './genius.com';
-export * from './www.jnsa.org';
-export * from './phpspot.org';
-export * from './www.infoq.com';
-export * from './www.moongift.jp';
-export * from './www.itmedia.co.jp';
-export * from './www.publickey1.jp';
-export * from './takagi-hiromitsu.jp';
-export * from './bookwalker.jp';
-export * from './www.yomiuri.co.jp';
-export * from './japan.cnet.com';
-export * from './deadline.com';
-export * from './www.gizmodo.jp';
-export * from './getnews.jp';
-export * from './www.lifehacker.jp';
-export * from './sect.iij.ad.jp';
-export * from './www.oreilly.co.jp';
-export * from './www.ipa.go.jp';
-export * from './weekly.ascii.jp';
-export * from './techlog.iij.ad.jp';
-export * from './wired.jp';
-export * from './japan.zdnet.com';
-export * from './www.rbbtoday.com';
-export * from './www.lemonde.fr';
-export * from './www.phoronix.com';
-export * from './pitchfork.com';
-export * from './biorxiv.org';
-export * from './epaper.zeit.de';
-export * from './www.ladbible.com';
-export * from './timesofindia.indiatimes.com';
-export * from './ma.ttias.be';
-export * from './pastebin.com';
-export * from './www.abendblatt.de';
-export * from './www.gruene.de';
-export * from './www.engadget.com';
-export * from './arstechnica.com';
-export * from './www.ndtv.com';
-export * from './www.spektrum.de';
-export * from './postlight.com';
-export * from './www.investmentexecutive.com';
-export * from './www.cbc.ca';
+const blogspot_com = require('./blogspot.com');
+const nymag_com = require('./nymag.com');
+const wikipedia_org = require('./wikipedia.org');
+const twitter_com = require('./twitter.com');
+const www_nytimes_com = require('./www.nytimes.com');
+const www_theatlantic_com = require('./www.theatlantic.com');
+const www_newyorker_com = require('./www.newyorker.com');
+const www_wired_com = require('./www.wired.com');
+const www_msn_com = require('./www.msn.com');
+const www_yahoo_com = require('./www.yahoo.com');
+const www_buzzfeed_com = require('./www.buzzfeed.com');
+const fandom_wikia_com = require('./fandom.wikia.com');
+const www_littlethings_com = require('./www.littlethings.com');
+const www_politico_com = require('./www.politico.com');
+const deadspin_com = require('./deadspin.com');
+const www_broadwayworld_com = require('./www.broadwayworld.com');
+const www_apartmenttherapy_com = require('./www.apartmenttherapy.com');
+const medium_com = require('./medium.com');
+const www_tmz_com = require('./www.tmz.com');
+const www_washingtonpost_com = require('./www.washingtonpost.com');
+const www_huffingtonpost_com = require('./www.huffingtonpost.com');
+const newrepublic_com = require('./newrepublic.com');
+const money_cnn_com = require('./money.cnn.com');
+const www_theverge_com = require('./www.theverge.com');
+const www_cnn_com = require('./www.cnn.com');
+const www_aol_com = require('./www.aol.com');
+const www_youtube_com = require('./www.youtube.com');
+const www_theguardian_com = require('./www.theguardian.com');
+const www_sbnation_com = require('./www.sbnation.com');
+const www_bloomberg_com = require('./www.bloomberg.com');
+const www_bustle_com = require('./www.bustle.com');
+const www_npr_org = require('./www.npr.org');
+const www_recode_net = require('./www.recode.net');
+const qz_com = require('./qz.com');
+const www_dmagazine_com = require('./www.dmagazine.com');
+const www_reuters_com = require('./www.reuters.com');
+const mashable_com = require('./mashable.com');
+const www_chicagotribune_com = require('./www.chicagotribune.com');
+const www_vox_com = require('./www.vox.com');
+const news_nationalgeographic_com = require('./news.nationalgeographic.com');
+const www_nationalgeographic_com = require('./www.nationalgeographic.com');
+const www_latimes_com = require('./www.latimes.com');
+const pagesix_com = require('./pagesix.com');
+const thefederalistpapers_org = require('./thefederalistpapers.org');
+const www_cbssports_com = require('./www.cbssports.com');
+const www_msnbc_com = require('./www.msnbc.com');
+const www_thepoliticalinsider_com = require('./www.thepoliticalinsider.com');
+const www_mentalfloss_com = require('./www.mentalfloss.com');
+const abcnews_go_com = require('./abcnews.go.com');
+const www_nydailynews_com = require('./www.nydailynews.com');
+const www_cnbc_com = require('./www.cnbc.com');
+const www_popsugar_com = require('./www.popsugar.com');
+const observer_com = require('./observer.com');
+const people_com = require('./people.com');
+const www_usmagazine_com = require('./www.usmagazine.com');
+const www_rollingstone_com = require('./www.rollingstone.com');
+const 247sports_com = require('./247sports.com');
+const uproxx_com = require('./uproxx.com');
+const www_eonline_com = require('./www.eonline.com');
+const www_miamiherald_com = require('./www.miamiherald.com');
+const www_refinery29_com = require('./www.refinery29.com');
+const www_macrumors_com = require('./www.macrumors.com');
+const www_androidcentral_com = require('./www.androidcentral.com');
+const www_si_com = require('./www.si.com');
+const www_rawstory_com = require('./www.rawstory.com');
+const www_cnet_com = require('./www.cnet.com');
+const www_today_com = require('./www.today.com');
+const www_al_com = require('./www.al.com');
+const www_thepennyhoarder_com = require('./www.thepennyhoarder.com');
+const www_westernjournalism_com = require('./www.westernjournalism.com');
+const www_americanow_com = require('./www.americanow.com');
+const sciencefly_com = require('./sciencefly.com');
+const hellogiggles_com = require('./hellogiggles.com');
+const thoughtcatalog_com = require('./thoughtcatalog.com');
+const www_inquisitr_com = require('./www.inquisitr.com');
+const www_nbcnews_com = require('./www.nbcnews.com');
+const fortune_com = require('./fortune.com');
+const www_linkedin_com = require('./www.linkedin.com');
+const obamawhitehouse_archives_gov = require('./obamawhitehouse.archives.gov');
+const www_opposingviews_com = require('./www.opposingviews.com');
+const www_prospectmagazine_co_uk = require('./www.prospectmagazine.co.uk');
+const forward_com = require('./forward.com');
+const www_qdaily_com = require('./www.qdaily.com');
+const gothamist_com = require('./gothamist.com');
+const www_fool_com = require('./www.fool.com');
+const www_slate_com = require('./www.slate.com');
+const ici_radio_canada_ca = require('./ici.radio-canada.ca');
+const www_fortinet_com = require('./www.fortinet.com');
+const www_fastcompany_com = require('./www.fastcompany.com');
+const blisterreview_com = require('./blisterreview.com');
+const news_mynavi_jp = require('./news.mynavi.jp');
+const clinicaltrials_gov = require('./clinicaltrials.gov');
+const github_com = require('./github.com');
+const www_reddit_com = require('./www.reddit.com');
+const otrs_com = require('./otrs.com');
+const www_ossnews_jp = require('./www.ossnews.jp');
+const buzzap_jp = require('./buzzap.jp');
+const www_asahi_com = require('./www.asahi.com');
+const www_sanwa_co_jp = require('./www.sanwa.co.jp');
+const www_elecom_co_jp = require('./www.elecom.co.jp');
+const scan_netsecurity_ne_jp = require('./scan.netsecurity.ne.jp');
+const jvndb_jvn_jp = require('./jvndb.jvn.jp');
+const genius_com = require('./genius.com');
+const www_jnsa_org = require('./www.jnsa.org');
+const phpspot_org = require('./phpspot.org');
+const www_infoq_com = require('./www.infoq.com');
+const www_moongift_jp = require('./www.moongift.jp');
+const www_itmedia_co_jp = require('./www.itmedia.co.jp');
+const www_publickey1_jp = require('./www.publickey1.jp');
+const takagi_hiromitsu_jp = require('./takagi-hiromitsu.jp');
+const bookwalker_jp = require('./bookwalker.jp');
+const www_yomiuri_co_jp = require('./www.yomiuri.co.jp');
+const japan_cnet_com = require('./japan.cnet.com');
+const deadline_com = require('./deadline.com');
+const www_gizmodo_jp = require('./www.gizmodo.jp');
+const getnews_jp = require('./getnews.jp');
+const www_lifehacker_jp = require('./www.lifehacker.jp');
+const sect_iij_ad_jp = require('./sect.iij.ad.jp');
+const www_oreilly_co_jp = require('./www.oreilly.co.jp');
+const www_ipa_go_jp = require('./www.ipa.go.jp');
+const weekly_ascii_jp = require('./weekly.ascii.jp');
+const techlog_iij_ad_jp = require('./techlog.iij.ad.jp');
+const wired_jp = require('./wired.jp');
+const japan_zdnet_com = require('./japan.zdnet.com');
+const www_rbbtoday_com = require('./www.rbbtoday.com');
+const www_lemonde_fr = require('./www.lemonde.fr');
+const www_phoronix_com = require('./www.phoronix.com');
+const pitchfork_com = require('./pitchfork.com');
+const biorxiv_org = require('./biorxiv.org');
+const epaper_zeit_de = require('./epaper.zeit.de');
+const www_ladbible_com = require('./www.ladbible.com');
+const timesofindia_indiatimes_com = require('./timesofindia.indiatimes.com');
+const ma_ttias_be = require('./ma.ttias.be');
+const pastebin_com = require('./pastebin.com');
+const www_abendblatt_de = require('./www.abendblatt.de');
+const www_gruene_de = require('./www.gruene.de');
+const www_engadget_com = require('./www.engadget.com');
+const arstechnica_com = require('./arstechnica.com');
+const www_ndtv_com = require('./www.ndtv.com');
+const www_spektrum_de = require('./www.spektrum.de');
+const postlight_com = require('./postlight.com');
+const www_investmentexecutive_com = require('./www.investmentexecutive.com');
+const www_cbc_ca = require('./www.cbc.ca');
+
+module.exports = {
+  ...blogspot_com,
+  ...nymag_com,
+  ...wikipedia_org,
+  ...twitter_com,
+  ...www_nytimes_com,
+  ...www_theatlantic_com,
+  ...www_newyorker_com,
+  ...www_wired_com,
+  ...www_msn_com,
+  ...www_yahoo_com,
+  ...www_buzzfeed_com,
+  ...fandom_wikia_com,
+  ...www_littlethings_com,
+  ...www_politico_com,
+  ...deadspin_com,
+  ...www_broadwayworld_com,
+  ...www_apartmenttherapy_com,
+  ...medium_com,
+  ...www_tmz_com,
+  ...www_washingtonpost_com,
+  ...www_huffingtonpost_com,
+  ...newrepublic_com,
+  ...money_cnn_com,
+  ...www_theverge_com,
+  ...www_cnn_com,
+  ...www_aol_com,
+  ...www_youtube_com,
+  ...www_theguardian_com,
+  ...www_sbnation_com,
+  ...www_bloomberg_com,
+  ...www_bustle_com,
+  ...www_npr_org,
+  ...www_recode_net,
+  ...qz_com,
+  ...www_dmagazine_com,
+  ...www_reuters_com,
+  ...mashable_com,
+  ...www_chicagotribune_com,
+  ...www_vox_com,
+  ...news_nationalgeographic_com,
+  ...www_nationalgeographic_com,
+  ...www_latimes_com,
+  ...pagesix_com,
+  ...thefederalistpapers_org,
+  ...www_cbssports_com,
+  ...www_msnbc_com,
+  ...www_thepoliticalinsider_com,
+  ...www_mentalfloss_com,
+  ...abcnews_go_com,
+  ...www_nydailynews_com,
+  ...www_cnbc_com,
+  ...www_popsugar_com,
+  ...observer_com,
+  ...people_com,
+  ...www_usmagazine_com,
+  ...www_rollingstone_com,
+  ...247sports_com,
+  ...uproxx_com,
+  ...www_eonline_com,
+  ...www_miamiherald_com,
+  ...www_refinery29_com,
+  ...www_macrumors_com,
+  ...www_androidcentral_com,
+  ...www_si_com,
+  ...www_rawstory_com,
+  ...www_cnet_com,
+  ...www_today_com,
+  ...www_al_com,
+  ...www_thepennyhoarder_com,
+  ...www_westernjournalism_com,
+  ...www_americanow_com,
+  ...sciencefly_com,
+  ...hellogiggles_com,
+  ...thoughtcatalog_com,
+  ...www_inquisitr_com,
+  ...www_nbcnews_com,
+  ...fortune_com,
+  ...www_linkedin_com,
+  ...obamawhitehouse_archives_gov,
+  ...www_opposingviews_com,
+  ...www_prospectmagazine_co_uk,
+  ...forward_com,
+  ...www_qdaily_com,
+  ...gothamist_com,
+  ...www_fool_com,
+  ...www_slate_com,
+  ...ici_radio_canada_ca,
+  ...www_fortinet_com,
+  ...www_fastcompany_com,
+  ...blisterreview_com,
+  ...news_mynavi_jp,
+  ...clinicaltrials_gov,
+  ...github_com,
+  ...www_reddit_com,
+  ...otrs_com,
+  ...www_ossnews_jp,
+  ...buzzap_jp,
+  ...www_asahi_com,
+  ...www_sanwa_co_jp,
+  ...www_elecom_co_jp,
+  ...scan_netsecurity_ne_jp,
+  ...jvndb_jvn_jp,
+  ...genius_com,
+  ...www_jnsa_org,
+  ...phpspot_org,
+  ...www_infoq_com,
+  ...www_moongift_jp,
+  ...www_itmedia_co_jp,
+  ...www_publickey1_jp,
+  ...takagi_hiromitsu_jp,
+  ...bookwalker_jp,
+  ...www_yomiuri_co_jp,
+  ...japan_cnet_com,
+  ...deadline_com,
+  ...www_gizmodo_jp,
+  ...getnews_jp,
+  ...www_lifehacker_jp,
+  ...sect_iij_ad_jp,
+  ...www_oreilly_co_jp,
+  ...www_ipa_go_jp,
+  ...weekly_ascii_jp,
+  ...techlog_iij_ad_jp,
+  ...wired_jp,
+  ...japan_zdnet_com,
+  ...www_rbbtoday_com,
+  ...www_lemonde_fr,
+  ...www_phoronix_com,
+  ...pitchfork_com,
+  ...biorxiv_org,
+  ...epaper_zeit_de,
+  ...www_ladbible_com,
+  ...timesofindia_indiatimes_com,
+  ...ma_ttias_be,
+  ...pastebin_com,
+  ...www_abendblatt_de,
+  ...www_gruene_de,
+  ...www_engadget_com,
+  ...arstechnica_com,
+  ...www_ndtv_com,
+  ...www_spektrum_de,
+  ...postlight_com,
+  ...www_investmentexecutive_com,
+  ...www_cbc_ca
+};
