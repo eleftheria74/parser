705940abe26a57f98d89737fd2988ec4b4d607be
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-30 19:41:20 +0300

    chore: add script to fix invalid export syntax

---
+++ scripts/fix-invalid-exports.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+function walk(dir, callback) {
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    const fullPath = path.join(dir, file);
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory()) {
+      walk(fullPath, callback);
+    } else if (file.endsWith('.js')) {
+      callback(fullPath);
+    }
+  });
+}
+
+function fixExports(filePath) {
+  let content = fs.readFileSync(filePath, 'utf-8');
+  let modified = false;
+
+  // Pattern 1: module.exports = { default as something } from './file';
+  const pattern1 = /module\.exports\s*=\s*{\s*default\s+as\s+(\w+)\s*}\s*from\s*['"](.*?)['"];?/g;
+  content = content.replace(pattern1, (_, name, file) => {
+    modified = true;
+    return `const ${name} = require('${file}').default;\nmodule.exports = { ${name} };`;
+  });
+
+  if (modified) {
+    fs.writeFileSync(filePath, content, 'utf-8');
+    console.log(`✅ Fixed export in ${filePath}`);
+  }
+}
+
+walk('./src', fixExports);
+walk('./scripts', fixExports);
