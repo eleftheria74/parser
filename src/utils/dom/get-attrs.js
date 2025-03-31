86d8a57fd9e1d9f0d9fcc261c5e53a3f3424860a
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-31 19:26:34 +0300

    Update get-attrs.js

--- src/utils/dom/get-attrs.js
+++ src/utils/dom/get-attrs.js
@@ -1,1 +1,17 @@
-module.exports = require('../../../../utils/dom/get-attrs');
+module.exports = function getAttrs(node) {
+  const { attribs, attributes } = node;
+
+  if (!attribs && attributes) {
+    const attrs = Reflect.ownKeys(attributes).reduce((acc, index) => {
+      const attr = attributes[index];
+
+      if (!attr.name || !attr.value) return acc;
+
+      acc[attr.name] = attr.value;
+      return acc;
+    }, {});
+    return attrs;
+  }
+
+  return attribs;
+};
