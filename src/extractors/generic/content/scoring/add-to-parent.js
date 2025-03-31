f18debc4fd3a1bf49c8ef36ce0d11652547537e8
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-31 14:23:05 +0300

    Update add-to-parent.js

--- src/extractors/generic/content/scoring/add-to-parent.js
+++ src/extractors/generic/content/scoring/add-to-parent.js
@@ -1,11 +1,11 @@
-const addScore = require('./add-score');
+module.exports = async function addToParent(node, $, score) {
+  // Dynamic import για το addScore
+  const addScore = (await import('./add-score')).default;
 
-// Adds 1/4 of a child's score to its parent
-module.exports = function addToParent(node, $, score) {
   const parent = node.parent();
   if (parent) {
     addScore(parent, $, score * 0.25);
   }
 
   return node;
-}
+};
