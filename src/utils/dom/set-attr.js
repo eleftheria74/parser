5f8ca4a16e2342a7b1416ccf17994a05260860ad
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-31 21:24:14 +0300

    Update set-attr.js

--- src/utils/dom/set-attr.js
+++ src/utils/dom/set-attr.js
@@ -1,1 +1,9 @@
-module.exports = require('../../../utils/dom/set-attr');
+module.exports = function setAttr(node, attr, val) {
+  if (node.attribs) {
+    node.attribs[attr] = val;
+  } else if (node.attributes) {
+    node.setAttribute(attr, val);
+  }
+
+  return node;
+};
