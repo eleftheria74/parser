84d02ad9cec073e95e6b58471ea00524cec248a1
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-31 14:02:28 +0300

    Update get-or-init-score.js

--- src/extractors/generic/content/scoring/get-or-init-score.js
+++ src/extractors/generic/content/scoring/get-or-init-score.js
@@ -1,4 +1,7 @@
-const { getScore, scoreNode, getWeight, addToParent } = require('./index');
+const getScore = require('./get-score').default;
+const scoreNode = require('./score-node').default;
+const getWeight = require('./get-weight').default;
+const addToParent = require('./add-to-parent').default;
 
 // gets and returns the score if it exists
 // if not, initializes a score based on
