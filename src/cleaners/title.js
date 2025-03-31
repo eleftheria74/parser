ee0ba1132c225aa172995914b0ee0f90791ce685
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-31 18:57:57 +0300

    fix: lazy-load resolveSplitTitle in title.js to avoid circular dependency

--- src/cleaners/title.js
+++ src/cleaners/title.js
@@ -1,28 +1,23 @@
 const { stripTags } = require('../resource/utils/dom');
 const { normalizeSpaces } = require('../utils/text');
-
 const { TITLE_SPLITTERS_RE } = require('./constants');
 
-// Χρήση δυναμικής φόρτωσης για να αποφύγουμε κυκλική εξάρτηση
-let resolveSplitTitle;
+// Lazy-load για αποφυγή circular require
+function getResolveSplitTitle() {
+  return require('./index').resolveSplitTitle;
+}
+
 module.exports = function cleanTitle(title, { url, $ }) {
-  // Αν το title έχει |, :, ή - στη μέση, προσπαθούμε να το καθαρίσουμε
   if (TITLE_SPLITTERS_RE.test(title)) {
-    if (!resolveSplitTitle) {
-      resolveSplitTitle = require('./index').resolveSplitTitle; // Φορτώνεται μόνο όταν είναι απαραίτητο
-    }
-    title = resolveSplitTitle(title, url);
+    title = getResolveSplitTitle()(title, url);
   }
 
-  // Τελικός έλεγχος για να δούμε αν το τίτλο είναι πολύ μεγάλο ή μικρό
   if (title.length > 150) {
-    // Αν είναι πολύ μεγάλο, παίρνουμε το h1 από το έγγραφο αν υπάρχει
     const h1 = $('h1');
     if (h1.length === 1) {
       title = h1.text();
     }
   }
 
-  // Αφαιρούμε τα HTML tags από το τίτλο
   return normalizeSpaces(stripTags(title, $).trim());
 };
