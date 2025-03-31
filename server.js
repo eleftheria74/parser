bdbe6568424fd1ff18601b76aabb02b9b7973d82
eleftheria74 <eleftheria@outlook.com.gr>
2025-03-31 22:14:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Parser = require'./src/mercury');
+const Parser = require('./src/mercury');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
